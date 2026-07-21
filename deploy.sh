e516cdf1f539aad746355c9fff4ad27a06d409b6
Fayanzar <fayanzar@gmail.com>
2026-07-21 13:33:38 +0300

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ -f .env ]; then
+    set -o allexport
+    source .env
+    set +o allexport
+else
+    echo "Error: .env file not found."
+    exit 1
+fi
+
+dotnet clean
+dotnet tool restore
+dotnet fable clean --yes
+dotnet fable
+
+npm ci
+npm run build
+rsync -a --delete dist/ $FE_DIR
